779fb6f4c7b4dc40d4322e6a4ded7afc29e7254c
anilkumarpr <112877941+anilkumarpr@users.noreply.github.com>
2023-03-23 23:35:22 +0530

    Stable 2

--- variables.tf
+++ variables.tf
@@ -8,3 +8,21 @@ variable web_disp_vm_names {
     description= "vm names"
     default=["web_disp1","web_disp2"]
 }
+
+variable qas_ascs_vm_names {
+    type = list(string)
+    description= "vm names"
+    default=["ascs_vm1","ascs_vm2"]
+}
+
+variable qas_app_vm_names {
+    type = list(string)
+    description= "vm names"
+    default=["ascs_vm1","ascs_vm2"]
+}
+
+variable qas_db_vm_names {
+    type = list(string)
+    description= "vm names"
+    default=["db_vm1","db_vm2"]
+}
